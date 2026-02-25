e6b4675f6ad777e4e7a5789fb9755c1ce575e5d0
7adrii <adriloma007@gmail.com>
2026-02-25 19:03:33 +0100

    feat: add JSDoc comments to isRetro function for better documentation

--- src/utils/isRetro.js
+++ src/utils/isRetro.js
@@ -1,3 +1,10 @@
+/**
+ * Calcula si una consola es retro o no en funcion de si tiene mas de 20 años de antiguedad.
+ * @param {Date} releaseDate - Fecha de lanzamiento de la consola.
+ * @param {Date} referenceDate - Fecha de referencia para calcular la antiguedad.
+ * @returns {boolean} - Devuelve true si la consola es retro (tiene más de 20 años), false en caso contrario.
+ */
+
 function isRetro(releaseDate, referenceDate = new Date()) {
 
     let age = referenceDate.getFullYear() - releaseDate.getFullYear()
